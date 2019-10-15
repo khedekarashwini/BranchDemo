0fb314d47abf389b4de8be8a0ee0b6b5bf9b650e
khedekarashwini <ashwinikhedekar24@gmail.com>
2019-10-15 13:53:54 +0530

    New Branch Demo Project Added

---
+++ src/com/javawings/branchdemo/BranchDemo.java
@@ -0,0 +1,12 @@
+package com.javawings.branchdemo;
+
+public class BranchDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello java World........!");
+
+	}
+
+}
